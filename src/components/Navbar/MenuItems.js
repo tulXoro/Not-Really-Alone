513d33d5cb7caa461fc482724f2799cb50ba3a7a
Vindhya <54672134+cdvindhya@users.noreply.github.com>
2021-01-17 19:28:32 +0530

    Create MenuItems.js

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+//constant array of navbar items
+
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links' //className component
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links' //className component
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links' //className component
+    }
+    //{
+        //title: 'Extra Mobile',
+        //url: '#',
+        //cName: 'nav-links-mobile' //className component
+    //}
+]
