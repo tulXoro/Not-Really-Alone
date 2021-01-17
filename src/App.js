3dfddb9475c1e03dbab0bd13e6273150791e9180
Vindhya <54672134+cdvindhya@users.noreply.github.com>
2021-01-17 19:22:35 +0530

    App.js Front End Update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Forum from "./components/Forum/Forum"
+//import Main from "./Main";
 import './App.css';
-import firebase from 'firebase/app';
-
-import "firebase/auth";
-import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey:       "AIzaSyCmc7QC6RYFXHvI-rKaCeFuczH8IGkfntU",
-  authDomain: "werenotreallyalone.firebaseapp.com",
-  databaseURL: "https://werenotreallyalone-default-rtdb.firebaseio.com",
-  projectId: "werenotreallyalone",
-  storageBucket: "werenotreallyalone.appspot.com",
-  messagingSenderId: "443048600165",
-  appId: "1:443048600165:web:34012c4b24604cfd3ddc8c",
-  measurementId: "G-MYMPB8N49G"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Forum />
     </div>
   );
 }
